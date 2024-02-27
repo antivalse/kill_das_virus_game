03c535cb4a76eba0da004d9d982c5d7c9f34171c
Victor Svensson <victor.svensson@protonmail.com>
2024-02-27 21:34:54 +0100

    added game-info and rule-set, change column sizing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 import {
-	ClientToServerEvents,
-	ServerToClientEvents,
+  ClientToServerEvents,
+  ServerToClientEvents,
 } from "@shared/types/SocketTypes";
 import "./assets/scss/style.scss";
 
@@ -9,21 +9,22 @@ const SOCKET_HOST = import.meta.env.VITE_SOCKET_HOST;
 
 // Connect to Socket.IO Server
 console.log("Connecting to Socket.IO Server at:", SOCKET_HOST);
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST);
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+  io(SOCKET_HOST);
 
 // Listen for when connection is established
 socket.on("connect", () => {
-	console.log("💥 Connected to the server", SOCKET_HOST);
-	console.log("🔗 Socket ID:", socket.id);
+  console.log("💥 Connected to the server", SOCKET_HOST);
+  console.log("🔗 Socket ID:", socket.id);
 });
 
 // Listen for when server got tired of us
 socket.on("disconnect", () => {
-	console.log("💀 Disconnected from the server:", SOCKET_HOST);
+  console.log("💀 Disconnected from the server:", SOCKET_HOST);
 });
 
 // Listen for when we're reconnected (either due to our or the servers connection)
 socket.io.on("reconnect", () => {
-	console.log("🍽️ Reconnected to the server:", SOCKET_HOST);
-	console.log("🔗 Socket ID:", socket.id);
+  console.log("🍽️ Reconnected to the server:", SOCKET_HOST);
+  console.log("🔗 Socket ID:", socket.id);
 });
