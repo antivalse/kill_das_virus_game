d6ea1652a187560f972bed9189e6b51678075b57
Mattea <mattea.sax@gmail.com>
2024-03-05 21:57:22 +0100

    removed addPlayersToGame in game_service since players are added to game when game is created

--- backend/src/services/game_service.ts
+++ backend/src/services/game_service.ts
@@ -43,29 +43,6 @@ export const createGame = (waitingPlayers: Player[]) => {
 };
 
 /**
- * Add players to game
- * @param gameId id of game
- * @param players array of players to be added to game
- */
-
-// // Add players to game
-// export const addPlayersToGame = async (gameId: string, players: Player[]) => {
-// 	return prisma.game.update({
-// 		where: {
-// 			id: gameId,
-// 		},
-// 		data: {
-// 			players: {
-// 				connect: players.map((player) => ({ id: player.id })),
-// 			},
-// 		},
-// 		include: {
-// 			players: true,
-// 		},
-// 	});
-// };
-
-/**
  * Get a single game with players to see if players were added
  * @param gameId
  * @returns
