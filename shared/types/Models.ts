67f5aae365790c87199989dd6feaafb818a2e8c0
Mattea <mattea.sax@gmail.com>
2024-03-08 20:52:15 +0100

    added result and highscore interfaces to models.ts

--- shared/types/Models.ts
+++ shared/types/Models.ts
@@ -8,3 +8,20 @@ export interface Player {
   id: string;
   playername: string;
 }
+
+export interface Result {
+  id: string;
+  playerOneHighscore: Number;
+  playerTwoHighscore: Number;
+  playerOneName: String;
+  playerOnePoint: Number;
+  playerTwoName: String;
+  playerTwoPoint: Number;
+  timestamp: Number;
+}
+
+export interface Highscore {
+  id: string;
+  playername: string;
+  highscore: number;
+}
